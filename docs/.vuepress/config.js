8c12bfa1e9f766e294153f5814287ab12d4183b8
Ivan Zakharchanka <3axap4eHko@gmail.com>
2024-07-19 14:04:35 -0400

    favicon fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,14 +10,14 @@ export default defineUserConfig({
     '0-dependency, high-performance, reactive event handling library optimized for both browser and Node.js environments. This library introduces a robust and type-safe abstraction for handling events, reducing boilerplate and increasing code maintainability.',
 
   head: [
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
-    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#3a0839' }],
-    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/evnty/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/evnty/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/evnty/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/evnty/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/evnty/safari-pinned-tab.svg', color: '#3a0839' }],
+    ['link', { rel: 'shortcut icon', href: '/evnty/favicon.ico' }],
     ['meta', { name: 'msapplication-TileColor', content: '#3a0839' }],
-    ['meta', { name: 'msapplication-config', content: '/browserconfig.xml' }],
+    ['meta', { name: 'msapplication-config', content: '/evnty/browserconfig.xml' }],
     ['meta', { name: 'theme-color', content: '#424242' }],
   ],
 
