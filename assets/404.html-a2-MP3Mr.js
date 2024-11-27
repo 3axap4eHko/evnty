8daf0d7cb4e0bb67064ac28b273c24d4654e7e70
3axap4eHko <3axap4eHko@gmail.com>
2024-11-27 02:10:49 +0000

    Deploying to gh-pages from @ 3axap4eHko/evnty@47d78f0593524289ffed888969b26bd363cd6138 🚀

---
+++ assets/404.html-a2-MP3Mr.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as n,b as a}from"./app-B3tralD-.js";const l={};function r(s,t){return o(),n("div",null,t[0]||(t[0]=[a("p",null,"404 Not Found",-1)]))}const _=e(l,[["render",r],["__file","404.html.vue"]]),i=JSON.parse('{"path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"NotFound"},"headers":[],"git":{},"filePathRelative":null}');export{_ as comp,i as data};
