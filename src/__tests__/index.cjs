604cda94953a47064454f3ef7426bd10f54c6759
Ivan Zakharchanka <3axap4eHko@gmail.com>
2025-08-19 22:55:16 -0400

    removes old functions

--- src/__tests__/index.cjs
+++ src/__tests__/index.cjs
@@ -1,15 +1,10 @@
 const { strict: assert } = require('node:assert');
-const { Unsubscribe, Event, Sequence, Signal, removeValue, setTimeoutAsync, createEvent, createInterval, merge } = require('../../build/index.cjs');
+const { Unsubscribe, Event, Sequence, Signal, createEvent } = require('../../build/index.cjs');
 
 assert(typeof Unsubscribe === 'function');
 assert(typeof Event === 'function');
 assert(typeof Sequence === 'function');
 assert(typeof Signal === 'function');
-assert(typeof createEvent === 'function');
-assert(typeof createInterval === 'function');
-assert(typeof merge === 'function');
-assert(typeof removeValue === 'function');
-assert(typeof setTimeoutAsync === 'function');
 assert(createEvent() instanceof Event);
 
 console.log('CJS import test passed');
--- src/__tests__/index.mjs
+++ src/__tests__/index.mjs
@@ -1,15 +1,10 @@
 import { strict as assert } from 'node:assert';
-import { Unsubscribe, Event, Sequence, Signal, removeValue, setTimeoutAsync, createEvent, createInterval, merge } from '../../build/index.js';
+import { Unsubscribe, Event, Sequence, Signal, createEvent } from '../../build/index.js';
 
 assert(typeof Unsubscribe === 'function');
 assert(typeof Event === 'function');
 assert(typeof Sequence === 'function');
 assert(typeof Signal === 'function');
-assert(typeof createEvent === 'function');
-assert(typeof createInterval === 'function');
-assert(typeof merge === 'function');
-assert(typeof removeValue === 'function');
-assert(typeof setTimeoutAsync === 'function');
 assert(createEvent() instanceof Event);
 
 console.log('MJS import test passed');
